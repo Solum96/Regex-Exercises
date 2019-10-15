f2897493a7724c051869e4366959e24bbcfd21ad
Nils Lundell <nils.solumfaeste@gmail.com>
2019-10-15 15:34:31 +0200

    Started exercise 1
    added strings

--- Regex/Regex/Program.cs
+++ Regex/Regex/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Regex
@@ -10,6 +11,8 @@ namespace Regex
     {
         static void Main(string[] args)
         {
+            string input = "min katt har tre konkatenerade kanter";
+            string pattern = "k.t";
         }
     }
 }
