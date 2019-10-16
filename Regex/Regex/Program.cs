39b118ff08753d86e883b255599e086aa1e41fd0
Nils Lundell <nils.solumfaeste@gmail.com>
2019-10-16 13:30:18 +0200

    Added exercise 2+3

--- Regex/Regex/Program.cs
+++ Regex/Regex/Program.cs
@@ -12,10 +12,26 @@ namespace RegexTest
         static void Main(string[] args)
         {
             string input = "min katt har tre konkatenerade kanter";
-            string pattern = "kat";
+            /* ÖVNING 1
 
+            string pattern = "kat";
             Match match = Regex.Match(input, pattern);
-            Console.WriteLine(match.Success);
+            Console.WriteLine(match.Success); */
+
+            /* ÖVNING 2
+            string pattern = "(kat)|(kan)|(kon)";
+            var matches = Regex.Matches(input, pattern);
+            foreach (var match in matches)
+            {
+                Console.WriteLine(match);
+            } */
+
+            string pattern = "a.";
+            var matches = Regex.Matches(input, pattern);
+            foreach (var match in matches)
+            {
+                Console.WriteLine(match);
+            }
             Console.ReadKey();
         }
     }
