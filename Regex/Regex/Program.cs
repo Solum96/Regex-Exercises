a614ffa594f62f36b30dec977eb4adf860b3d3de
Nils Lundell <nils.solumfaeste@gmail.com>
2019-10-16 13:15:16 +0200

    Added exercise 1

--- Regex/Regex/Program.cs
+++ Regex/Regex/Program.cs
@@ -5,14 +5,18 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Regex
+namespace RegexTest
 {
     class Program
     {
         static void Main(string[] args)
         {
             string input = "min katt har tre konkatenerade kanter";
-            string pattern = "k.t";
+            string pattern = "kat";
+
+            Match match = Regex.Match(input, pattern);
+            Console.WriteLine(match.Success);
+            Console.ReadKey();
         }
     }
 }
