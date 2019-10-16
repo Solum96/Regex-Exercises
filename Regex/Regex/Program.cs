56cb8ddfcf442dc315c513dfbc4d46d4485ecb0b
Nils Lundell <nils.solumfaeste@gmail.com>
2019-10-16 14:30:57 +0200

    added ex6+7

--- Regex/Regex/Program.cs
+++ Regex/Regex/Program.cs
@@ -12,6 +12,7 @@ namespace RegexTest
         static void Main(string[] args)
         {
             string input = "min katt har tre konkatenerade kanter";
+            string input2 = "hands jumbled terrific old-fashioned grass highfalutin stupendous daughter feigned skirt modern truthful";
             /* ÖVNING 1
 
             string pattern = "kat";
@@ -41,13 +42,38 @@ namespace RegexTest
                 Console.WriteLine(match);
             } */
 
-
-            string pattern = "[^t]t[^t]";
-            var matches = Regex.Matches(input, pattern);
+            // ÖVNING 5
+            /*
+            string pattern = "(as)|(od)";
+            var matches = Regex.Matches(input2, pattern);
             foreach (var match in matches)
             {
                 Console.WriteLine(match);
+            } */
+
+            /* ÖVNING 6
+            string pattern = "([ae][nds])";
+            var matches = Regex.Matches(input2, pattern);
+            foreach (var match in matches)
+            {
+                Console.WriteLine(match);
+            } */
+
+            /* ÖVNING 7
+            string pattern1 = @"\b([hd]\w*)\b";
+            string pattern2 = @"\w+s\b";
+            var matches1 = Regex.Matches(input2, pattern1);
+            var matches2 = Regex.Matches(input2, pattern2);
+            foreach (var match in matches1)
+            {
+                Console.WriteLine(match);
             }
+            foreach (var match in matches2)
+            {
+                Console.WriteLine(match);
+            } */
+
+
             Console.ReadKey();
         }
     }
